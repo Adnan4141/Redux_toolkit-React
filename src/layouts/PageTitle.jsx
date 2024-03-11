66ef74823ff948df65bcee99a9fda04e32dae7da
Adnanh4141 <adnanh4141@gmail.com>
2024-03-12 00:24:36 +0600

    Favicon added

--- src/layouts/PageTitle.jsx
+++ src/layouts/PageTitle.jsx
@@ -1,10 +1,12 @@
 import {Helmet} from "react-helmet";
+import { SiAdonisjs } from "react-icons/si";
 
 
 const PageTitle = ({title}) => {
   return (
     <Helmet>
       <title>{title}</title>
+      <link rel="icon" type="<SiAdonisjs />" href="../../assets/download.jpeg" />
     </Helmet>
   );
 };
